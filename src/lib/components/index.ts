c4d781bfe1c4f35f5dc0816a830ab9ec648b5c72
freitzzz <1160907@isep.ipp.pt>
2023-01-12 22:54:27 +0000

    layout for notify button

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,1 +1,2 @@
 export { default as SearchInput } from './search_input.svelte';
+export { default as NotifyButton } from './notify_button.svelte';
---
+++ src/lib/components/notify_button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let id: string;
+</script>
+
+<button id="notify-button-{id}" on:click>
+	<div style="display: flex; flex-direction: row;">
+		<div>Receber notificações</div>
+		<div>🔔</div>
+	</div>
+</button>
