c02b5728173d68fe4dc00f111766f168942cc894
freitzzz <1160907@isep.ipp.pt>
2023-01-12 22:55:27 +0000

    progress on homepage layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { NotifyButton, SearchInput } from '@components';
+
+	let value: string;
+</script>
+
+<center>
+	<h1>Alguém sabe as minhas credenciais?</h1>
+	<h2>Utiliza o nosso motor de busca e descobre se foste afetado!</h2>
+
+	<SearchInput id="1234" bind:value />
+
+	{#if value}
+		<h3>
+			Não encontro registos para: {value}
+		</h3>
+	{/if}
+</center>
+
+<div style="text-align: right; vertical-align: bottom;">
+	<NotifyButton id="1234" />
+</div>
