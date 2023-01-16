4526f707d9922b8ef12860d160a5198ee9263c20
freitzzz <1160907@isep.ipp.pt>
2023-01-16 21:03:39 +0000

    include homepage strings

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,8 +1,9 @@
 import type { Translation } from '../i18n-types'
 
 const en: Translation = {
-	// this is an example Translation, just rename or delete this folder if you want
-	HI: 'Hallo {name}! Bitte hinterlasse einen Stern, wenn dir das Projekt gefällt: https://github.com/ivanhofer/typesafe-i18n',
+	homepageTitle: 'Alguém sabe as minhas credenciais? (EN)',
+	homepageDescription: 'Utiliza o nosso motor de busca e descobre se foste afetado! (EN)',
+
 }
 
 export default en
