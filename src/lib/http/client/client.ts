287d6e2fd25eb9f23163695e3951585db44e1206
freitzzz <1160907@isep.ipp.pt>
2023-01-17 20:18:11 +0000

    support query parameters in fetch client

--- src/lib/http/client/client.ts
+++ src/lib/http/client/client.ts
@@ -1,24 +1,46 @@
 export interface Client {
-	get(endpoint: string): Promise<Response>;
+	get(endpoint: string, query?: QueryParameters): Promise<Response>;
 }
 
 export class FetchClient implements Client {
 	constructor(private readonly baseUrl: URL) {}
 
-	get(endpoint: string): Promise<Response> {
-		return this.request(endpoint, Method.get);
+	get(endpoint: string, query?: QueryParameters): Promise<Response> {
+		const request = <Request>{
+			url: this.resolveUrl(this.baseUrl, endpoint, query),
+			method: Method.get,
+			query: query
+		};
+		return this.request(request);
 	}
 
-	private request(endpoint: string, method: Method): Promise<Response> {
-		const url = `${this.baseUrl}${endpoint}`;
+	private request(request: Request): Promise<Response> {
+		const url = request.url;
 
-		const request = <RequestInit>{
-			method: method
+		const fetchRequest = <RequestInit>{
+			method: request.method
 		};
 
-		return fetch(url, request);
+		return fetch(url, fetchRequest);
+	}
+
+	private resolveUrl(baseUrl: URL, endpoint: string, query?: QueryParameters): string {
+		let url = `${baseUrl}${endpoint}`.replaceAll(/\/{2,}/g, '/');
+
+		if (query) {
+			url = `${url}?${new URLSearchParams(query)}`;
+		}
+
+		return url;
 	}
 }
+
+export type QueryParameters = Record<string, string>;
+
+type Request = {
+	url: string;
+	method: Method;
+};
 
 enum Method {
 	get = 'GET'
