68fdc12292137740bff7abacdc15cb4fa2864240
freitzzz <1160907@isep.ipp.pt>
2023-01-17 21:13:49 +0000

    run format

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,10 +1,10 @@
-import { detectLocale, loadLocaleAsync, setLocale } from "@i18n";
-import type { PageLoad } from "./$types";
+import { detectLocale, loadLocaleAsync, setLocale } from '@i18n';
+import type { PageLoad } from './$types';
 
 export const load = (async () => {
-    const locale = detectLocale();
+	const locale = detectLocale();
 
-    await loadLocaleAsync(locale);
+	await loadLocaleAsync(locale);
 
-    setLocale(locale);
+	setLocale(locale);
 }) satisfies PageLoad;
