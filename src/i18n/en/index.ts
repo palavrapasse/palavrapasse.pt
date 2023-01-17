fcca337b0965d303579cd03c2adc4072fe94f9c3
freitzzz <1160907@isep.ipp.pt>
2023-01-17 20:41:36 +0000

    npm format

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -1,9 +1,8 @@
-import type { Translation } from '../i18n-types'
+import type { Translation } from '../i18n-types';
 
 const en: Translation = {
 	homepageTitle: 'Alguém sabe as minhas credenciais? (EN)',
-	homepageDescription: 'Utiliza o nosso motor de busca e descobre se foste afetado! (EN)',
+	homepageDescription: 'Utiliza o nosso motor de busca e descobre se foste afetado! (EN)'
+};
 
-}
-
-export default en
+export default en;
--- src/i18n/pt/index.ts
+++ src/i18n/pt/index.ts
@@ -1,8 +1,8 @@
-import type { BaseTranslation } from '../i18n-types'
+import type { BaseTranslation } from '../i18n-types';
 
 const pt: BaseTranslation = {
 	homepageTitle: 'Alguém sabe as minhas credenciais?',
-	homepageDescription: 'Utiliza o nosso motor de busca e descobre se foste afetado!',
-}
+	homepageDescription: 'Utiliza o nosso motor de busca e descobre se foste afetado!'
+};
 
-export default pt
+export default pt;
