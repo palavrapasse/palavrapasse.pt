50e6229bbb55c3bc3ff9e681cd8d5a682b09d8ad
freitzzz <1160907@isep.ipp.pt>
2023-01-19 20:14:31 +0000

    include query leaks table component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,5 @@
 export { default as SearchInput } from './search_input.svelte';
 export { default as NotifyButton } from './notify_button.svelte';
-export { default as LeaksQueryTable } from './leaks_query_table.svelte';
+export { default as QueryLeaksTable } from './query_leaks_table.svelte';
 export { default as ThumbsUpIllustration } from './thumbs_up_illustration.svelte';
 export { default as HelloIllustration } from './hello_illustration.svelte';
---
+++ src/lib/components/query_leaks_table.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { QueryLeaks } from '@http';
+
+	export let id: string;
+	export let leaks: QueryLeaks;
+</script>
+
+<div class="flex w-full overflow-x-auto">
+	<table id="leaks-query-table-{id}" class="table">
+		<thead>
+			<tr>
+				<th>Contexto</th>
+				<th>Data do Leak</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each leaks as leak}
+				<tr>
+					<th>{leak.context}</th>
+					<td>{new Date(leak.shareDateMSEpoch).toLocaleDateString()}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
