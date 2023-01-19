03f670c9a2ed7e063e5233deee5d19abc4936b88
freitzzz <1160907@isep.ipp.pt>
2023-01-19 21:47:05 +0000

    layout for use case #001

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,72 @@
 <script lang="ts">
-	import { NotifyButton, SearchInput } from '@components';
+	import '../app.css';
+	import { LL } from '@i18n';
+	import {
+		NotifyButton,
+		SearchInput,
+		QueryLeaksTable,
+		HelloIllustration,
+		ThumbsUpIllustration
+	} from '@components';
+	import { QueryLeaksStore, State } from '@stores';
 
 	let value: string;
+	let affected: string[];
+
+	$: {
+		if (value) {
+			affected = [];
+		}
+	}
+
+	function searchAffected(): void {
+		const trimValue = value.trim();
+
+		if (trimValue.length > 0) {
+			affected = [value];
+
+			QueryLeaksStore.affected(affected);
+		}
+	}
 </script>
 
-<center>
-	<h1>Alguém sabe as minhas credenciais?</h1>
-	<h2>Utiliza o nosso motor de busca e descobre se foste afetado!</h2>
+<body class="md:container md:mx-auto flex flex-col items-center">
+	<h1 class="text-4xl h-12">{$LL.homepageTitle()}</h1>
+	<h2 class="text-xl h-16">{$LL.homepageDescription()}</h2>
 
-	<SearchInput id="1234" bind:value />
+	<!-- preventDefault prevents input being included in webpage url -->
+	<form class="h-14" on:submit|preventDefault={searchAffected}>
+		<SearchInput id="affected-email" bind:value />
+	</form>
 
-	{#if value}
-		<h3>
-			Não encontro registos para: {value}
-		</h3>
+	{#if $QueryLeaksStore.state == State.success}
+		{#if $QueryLeaksStore.value.length > 0}
+			<QueryLeaksTable id={value} leaks={$QueryLeaksStore.value} />
+		{:else}
+			{$LL.leaksQueryNotFoundResponse({ email: value })}
+			<ThumbsUpIllustration />
+		{/if}
+	{:else if $QueryLeaksStore.state == State.loading}
+		<div class="text-center">
+			<svg
+				class="animate-spin text-center h-3/4"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M6 18L18 6M6 6l12 12"
+				/>
+			</svg>
+		</div>
+	{:else}
+		<HelloIllustration />
 	{/if}
-</center>
+</body>
 
-<div style="text-align: right; vertical-align: bottom;">
-	<NotifyButton id="1234" />
+<div class="fixed bottom-12 right-16">
+	<NotifyButton id="subscribe-leak-notifications" />
 </div>
