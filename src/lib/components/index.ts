1c9e2e432f9a3266f68be9234317fb9a154ca037
freitzzz <1160907@isep.ipp.pt>
2023-01-26 22:15:41 +0000

    include drop down for query leaks target

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -5,3 +5,4 @@ export { default as QueryLeaksTable } from './query_leaks_table.svelte';
 export { default as SearchInput } from './search_input.svelte';
 export { default as ThumbsUpIllustration } from './thumbs_up_illustration.svelte';
 export { default as Dropdown } from './dropdown.svelte';
+export { default as QueryLeaksTargetDropdown } from './query_leaks_target_dropdown.svelte';
---
+++ src/lib/components/query_leaks_target_dropdown.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Target } from '@http';
+	import { i18nTarget, LL } from '@i18n';
+	import Dropdown from './dropdown.svelte';
+
+	export let onValueChange: (value: Target) => void;
+
+	const values: [Target, string][] = Object.values(Target).map((t) => [t, i18nTarget($LL, t)]);
+</script>
+
+<Dropdown id="query-leaks-target" label="Filtro" {values} {onValueChange} />
