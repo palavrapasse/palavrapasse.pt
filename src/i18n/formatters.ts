08799ad2afa7c72caf15a6e789956b064cd3066d
freitzzz <1160907@isep.ipp.pt>
2023-01-27 22:33:15 +0000

    revert /* eslint-disable @typescript-eslint/no-unused-vars */

--- src/i18n/formatters.ts
+++ src/i18n/formatters.ts
@@ -1,6 +1,7 @@
 import type { FormattersInitializer } from 'typesafe-i18n';
 import type { Locales, Formatters } from './i18n-types';
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export const initFormatters: FormattersInitializer<Locales, Formatters> = (locale: Locales) => {
 	const formatters: Formatters = {
 		date: dateFormatter
