1a3663939fd8fbc12a656315238829288910a00e
freitzzz <1160907@isep.ipp.pt>
2023-01-27 22:33:40 +0000

    include checkers for default leak information

--- src/lib/http/model/leak.ts
+++ src/lib/http/model/leak.ts
@@ -1,3 +1,9 @@
+const combolistLeakContext = 'COMBOLIST';
+const defaultLeakBadActor = 'UNKNOWN';
+const defaultLeakContext = [combolistLeakContext, 'UNKNOWN'];
+const defaultLeakPlatform = 'UNKNOWN';
+const defaultLeakShareDate = new Date(0);
+
 export type Leak = {
 	email: string;
 	context: string;
@@ -5,3 +11,23 @@ export type Leak = {
 };
 
 export type QueryLeaks = Array<Leak>;
+
+export function isDefaultBadActor(actor: string): boolean {
+	return actor === defaultLeakBadActor;
+}
+
+export function isDefaultContext(context: string): boolean {
+	return defaultLeakContext.includes(context);
+}
+
+export function isDefaultPlatform(platform: string): boolean {
+	return platform === defaultLeakPlatform;
+}
+
+export function isDefaultShareDate(shareDate: Date): boolean {
+	return shareDate.getTime() === defaultLeakShareDate.getTime();
+}
+
+export function isCombolistContext(context: string): boolean {
+	return context.toUpperCase().includes(combolistLeakContext);
+}
