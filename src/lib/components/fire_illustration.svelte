ac265c4893337bb432f5f660815b8845225b2541
freitzzz <1160907@isep.ipp.pt>
2023-02-07 23:04:49 +0000

    include fire, nope and searching illustrations

---
+++ src/lib/components/fire_illustration.svelte
@@ -0,0 +1,6 @@
+<img
+	id="fire-illustration"
+	class="h-96"
+	src="svg/chubby_fire.svg"
+	alt="cartoonish character with his head on fire showing an anger face"
+/>
---
+++ src/lib/components/nope_illustration.svelte
@@ -0,0 +1,6 @@
+<img
+	id="nope-illustration"
+	class="h-96"
+	src="svg/chubby_nope.svg"
+	alt="cartoonish character raising his arms with a confused face"
+/>
